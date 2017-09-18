5896b6a2e1fe3ad05f97c1bc31818bbf2b0424c7
mnyorba <nrb@mail.ru>
2017-09-18 13:11:07 +0300

    Yeoman generator for a WordPress starter theme using Underscores with npm
    Signed-off-by: mnyorba <nrb@mail.ru>

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # generator-npm-wp-s-theme
-Yeoman generator with npm for a WordPress starter theme using Underscores
+
+[Yeoman](http://yeoman.io/) generator for a WordPress starter theme (using [Underscores](https://github.com/Automattic/_s)) with [npm](https://www.npmjs.com/) and other good stuff. This will install the last version of `_s` (Underscores) and optionally a npm package setup ready for development and production.
+
+## List of packages used
+[autoprefixer](https://github.com/postcss/autoprefixer), [browser-sync](https://github.com/Browsersync/browser-sync), [eslint](https://github.com/eslint/eslint), [imagemin-cli](https://github.com/imagemin/imagemin-cli), [node-sass](https://github.com/sass/node-sass), [onchange](https://github.com/Qard/onchange).
+
+## Installation
+
+* First, ensure that node.js & npm are both installed. If not, choose your OS and installation method from [this page](https://nodejs.org/en/download/package-manager/) and follow the instructions.
+
+##### Install required tools `yo`:
+
+```bash
+npm install -g yo
+```
+
+##### Install `generator-npm-wp-s-theme`:
+
+```bash
+npm install -g generator-npm-wp-s-theme
+```
+
+### Run
+
+##### Create a new directory, and go into:
+
+```bash
+mkdir my-new-theme && cd $_
+```
+
+##### Run `yo npm-wp-s-theme`, and fill the info:
+
+```bash
+yo npm-wp-s-theme
+```
+
+## Usage
+### `serve`
+  `browser-sync start --server --files 'dist/css/*.css, dist/js/*.js, **/*.html, !node_modules/**/*.html'`
+
+  Start a new server and watch for CSS & JS file changes in the `dist` folder
+
+### `build:css`
+  `run-s scss autoprefixer`
+
+  Alias to run the `scss` and `autoprefixer` tasks. Compiles Scss to CSS & add vendor prefixes
+
+### `build:js`
+  `run-s lint concat uglify`
+
+  Alias to run the `lint`, `concat` and `uglify` tasks. Lints JS, combines `src` JS files & uglifies the output
+
+### `build:images`
+  `run-s imagemin icons`
+
+  Alias to run the `imagemin` and `icons` tasks. Compresses images, generates an SVG sprite from a folder of separate SVGs
+
+### `build`
+  `run-s build:*`
+
+  Alias to run all of the `build` commands
+
+### `watch:css`
+  `onchange 'src/**/*.scss' -- run-s build:css`
+
+  Watches for any .scss file in `src` to change, then runs the `build:css` task
+
+### `watch:js`
+  `onchange 'src/**/*.js' -- run-s build:js`
+
+  Watches for any .js file in `src` to change, then runs the `build:js` task
+
+### `watch:images`
+  `onchange 'src/images/**/*' -- run-s build:images`
+
+  Watches for any images in `src` to change, then runs the `build:images` task
+
+### `watch`
+  `run-p serve watch:*`
+
+  Run the following tasks simultaneously: `serve`, `watch:css`, `watch:js` & `watch:images`. When a .scss or .js file changes in `src` or an image changes in `src/images`, the task will compile the changes to `dist`, and the server will be notified of the change. Any browser connected to the server will then inject the new file from `dist`
+
+
+## Bower usage
+- Run `bower install --save <package>` to install frontend dependencies
+- Run `wiredep` to include SCSS file in `/sass/style.scss`
+
+## Versioning
+
+Maintained under the [Semantic Versioning guidelines](http://semver.org/).
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
+Copyright (c) 2017 [MNyorba]
---
+++ app/index.js
@@ -0,0 +1,365 @@
+var generators = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var _ = require('lodash');
+var fs = require('fs');
+var del = require('del');
+var download = require('download');
+var downloadStatus = require('download-status');
+var walk = require('walk');
+var path = require('path');
+
+module.exports = generators.Base.extend({
+  prompting: {
+    askToUser: function askToUser() {
+      var done = this.async();
+
+      this.log(yosay(
+        'Welcome to the kickass ' + chalk.red('wordpress-s-theme') + ' generator!'
+      ));
+
+      var prompts = [
+				{
+          type: 'input',
+          name: 'proxyname',
+          message: 'Localhost address',
+          default: 'localhost'
+        },
+				{
+          type: 'input',
+          name: 'domenname',
+          message: 'If you want to add a domain site',
+          default: ''
+        },
+        {
+          type: 'input',
+          name: 'themename',
+          message: 'Theme Name',
+          default: 'Theme Name'
+        },
+        {
+          type: 'input',
+          name: 'themeslug',
+          message: 'Theme Slug',
+          default: 'Theme Slug'
+        },
+        {
+          type: 'input',
+          name: 'themeuri',
+          message: 'Theme URI',
+          default: 'https://example.com/themeuri/'
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: 'Author',
+          default: 'Author'
+        },
+        {
+          type: 'input',
+          name: 'authoruri',
+          message: 'Author URI',
+          default: 'https://example.com/authoruri/'
+        },
+        {
+          type: 'input',
+          name: 'authoremail',
+          message: 'Author Email',
+          default: 'info@example.com'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Description',
+          default: 'Description'
+        },
+        {
+          type: 'input',
+          name: 'bugreport',
+          message: 'Bug Report',
+          default: 'https://example.com/bugreport/'
+        },
+        {
+          type: 'confirm',
+          name: 'gitignore',
+          message: 'Would you like to add a ' + chalk.white('.gitignore') + ' file?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'editorconfig',
+          message: 'Would you like to add a ' + chalk.white('.editorconfig') + ' file?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'jshintrc',
+          message: 'Would you like to add a ' + chalk.white('.jshintrc') + ' file?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'gulpsetup',
+          message: 'Would you like to setup a Gulp configuration ready to use?',
+          default: true
+        }
+      ];
+
+      this.prompt(prompts, function (props) {
+        this.props = props;
+        this.props.prefix = _.snakeCase(this.props.themeslug) + '_';
+        this.props.themeslug = _.kebabCase(this.props.themeslug);
+
+        done();
+      }.bind(this));
+    }
+  },
+
+  writing: {
+    installUnderscores: function installUnderscores() {
+      var done = this.async();
+      var callback = function (error, remote) {
+        if (error) {
+          done(error);
+        }
+
+        done();
+      }.bind(this);
+
+      this.log(chalk.yellow('\nLet\'s grab the latest version of Underscores...'));
+
+      new download({mode: '755', extract: true, strip: 1})
+        .get('https://github.com/Automattic/_s/archive/master.tar.gz')
+        .dest('.')
+        .use(downloadStatus())
+        .run(callback);
+    },
+
+    deleteFiles: function deleteFiles() {
+      var done = this.async();
+      var dir = this.destinationRoot();
+
+      var unusedFiles = ['.jscsrc', '.jshintignore', '.travis.yml', 'codesniffer.ruleset.xml', 'CONTRIBUTING.md', 'README.md'];
+
+      this.log(chalk.yellow('\nDeleting some unused files...'));
+
+      // Delete unused files
+      unusedFiles = _.map(unusedFiles, function(file) {
+        return dir + '/' + file;
+      });
+
+      del(unusedFiles).catch(function(error) {
+        done(error);
+      })
+      .then(paths => {
+        this.log(chalk.cyan(paths.join('\n')));
+
+        done();
+      });
+    },
+
+    parseThemeFiles: function parseThemeFiles() {
+      var done = this.async();
+      var _this = this;
+      var walker;
+
+      _this.log(chalk.yellow('\nParsing theme files...'));
+
+      walker = walk.walk('.');
+
+      _this.log.write().info('Please wait...');
+
+      walker.on("file", function (root, fileStats, next) {
+        var filePath = root + '/' + fileStats.name;
+
+        if (path.extname(fileStats.name) == '.php') {
+          fs.readFile(filePath, 'utf8', function (err,data) {
+            if (err) {
+              done(error);
+            }
+
+            var result;
+
+            result = data.replace(/'_s'/g, "'" + _this.props.themeslug + "'");
+            result = result.replace(/_s_/g, _this.props.prefix);
+            result = result.replace(/ _s/g, ' ' + _this.props.themename);
+            result = result.replace(/_s-/g, _this.props.themeslug + '-');
+
+            fs.writeFile(filePath, result, 'utf8', function (err) {
+              if (err) {
+                done(error);
+              }
+            });
+
+            next();
+          });
+        } else if (path.extname(fileStats.name) == '.css' || fileStats.name == 'style.scss') {
+          fs.readFile(filePath, 'utf8', function (err,data) {
+            if (err) {
+              done(error);
+            }
+
+            var result;
+
+            result = data.replace(/(Theme Name: )(.+)/g, '$1' + _this.props.themename);
+            result = result.replace(/(Theme URI: )(.+)/g, '$1' + _this.props.themeuri);
+            result = result.replace(/(Author: )(.+)/g, '$1' + _this.props.author);
+            result = result.replace(/(Author URI: )(.+)/g, '$1' + _this.props.authoruri);
+            result = result.replace(/(Description: )(.+)/g, '$1' + _this.props.description);
+            result = result.replace(/(Text Domain: )(.+)/g, '$1' + _this.props.themeslug);
+            result = result.replace(/_s is based on Underscores/g, _this.props.themename + ' is based on Underscores');
+            result = result.replace(/\@import "variables-site\/variables-site";/g, '\n\n// bower:scss' + '\n\n// endbower\n\n' + '\n\n@import "variables-site\/variables-site";');
+
+            fs.writeFile(filePath, result, 'utf8', function (err) {
+              if (err) {
+                done(error);
+              }
+            });
+
+            next();
+          });
+        } else if (fileStats.name === 'readme.txt') {
+          fs.readFile(filePath, 'utf8', function (err,data) {
+            if (err) {
+              done(error);
+            }
+
+            var result;
+
+            result = data.replace(/=== _s ===/g, '=== ' + _this.props.themename + ' ===');
+            result = result.replace(/A starter theme called _s, or underscores./g, _this.props.description);
+            result = result.replace(/(== Description ==\n\n)(.+)/g, '$1' + 'Long description here');
+            result = result.replace(/== Frequently Asked Questions ==[\s\S]*?== Credits ==/g, '== Credits ==');
+
+            fs.writeFile(filePath, result, 'utf8', function (err) {
+              if (err) {
+                done(error);
+              }
+            });
+
+            next();
+          });
+        } else if (fileStats.name === '_s.pot') {
+          fs.readFile(filePath, 'utf8', function (err,data) {
+            if (err) {
+              done(error);
+            }
+
+            var result;
+
+            result = data.replace(/Copyright (C) 2016 Automattic/g, 'Copyright (C) 2016 ' + _this.props.author);
+            result = result.replace(/Project-Id-Version: _s/g, 'Project-Id-Version: ' + _this.props.themename);
+            result = result.replace(/Report-Msgid-Bugs-To: https:\/\/wordpress.org\/tags\/_s/g, 'Report-Msgid-Bugs-To: ' + _this.props.bugreport);
+            result = result.replace(/Language-Team: LANGUAGE <LL@li\.org>/g, 'Language-Team: ' + _this.props.author + '<' + _this.props.authoremail + '>');
+            result = result.replace(/@ _s/g, '@ ' + _this.props.themeslug);
+
+            fs.writeFile(filePath, result, 'utf8', function (err) {
+              if (err) {
+                done(error);
+              }
+            });
+
+            fs.rename(filePath, './languages/' + _this.props.themeslug + '.pot');
+
+            next();
+          });
+        } else {
+          next();
+        }
+      });
+
+      walker.on("errors", function (root, nodeStatsArray, next) {
+        done(error);
+        next();
+      });
+
+      walker.on("end", function () {
+        done();
+      });
+    },
+
+    packageFiles: function packageFiles() {
+      this.log(chalk.yellow('\nCopying configuration files...'));
+
+      if (this.props.gitignore) {
+        this.fs.copy(
+          this.templatePath('_gitignore'),
+          this.destinationPath('.gitignore')
+        );
+      }
+
+      if (this.props.editorconfig) {
+        this.fs.copy(
+          this.templatePath('_editorconfig'),
+          this.destinationPath('.editorconfig')
+        );
+      }
+
+      if (this.props.jshintrc) {
+        this.fs.copy(
+          this.templatePath('_jshintrc'),
+          this.destinationPath('.jshintrc')
+        );
+      }
+
+      if (this.props.gulpsetup) {
+        this.fs.copyTpl(
+          this.templatePath('_package.json'),
+          this.destinationPath('package.json'),
+          {
+            package_name: this.props.themeslug,
+            package_description: this.props.description,
+            package_author: this.props.author,
+            proxy_address: this.props.proxyname,
+            proxy_domain: this.props.domenname,
+            theme_domain: this.props.themeslug,
+            package_name: this.props.themename,
+            theme_bugreport: this.props.bugreport,
+            author: this.props.author,
+            author_email: this.props.authoremail
+          }
+        );
+      }
+    }
+  },
+
+  install: {
+    installPackages: function installPackages() {
+      if (this.props.gulpsetup) {
+        this.log(chalk.yellow('\nInstalling required packages...'));
+
+        this.npmInstall(['autoprefixer'], { 'saveDev': true });
+        this.npmInstall(['browser-sync'], { 'saveDev': true });
+        this.npmInstall(['eslint'], { 'saveDev': true });
+        this.npmInstall(['eslint-config-standard'], { 'saveDev': true });
+        this.npmInstall(['eslint-plugin-promise'], { 'saveDev': true });
+        this.npmInstall(['eslint-plugin-standard'], { 'saveDev': true });
+        this.npmInstall(['imagemin-cli'], { 'saveDev': true });
+        this.npmInstall(['mkdirp'], { 'saveDev': true });
+        this.npmInstall(['node-sass'], { 'saveDev': true });
+        this.npmInstall(['npm-run-all'], { 'saveDev': true });
+        this.npmInstall(['onchange'], { 'saveDev': true });
+        this.npmInstall(['postcss-cli'], { 'saveDev': true });
+        this.npmInstall(['rimraf'], { 'saveDev': true });
+        this.npmInstall(['stylelint'], { 'saveDev': true });
+        this.npmInstall(['uglify-js'], { 'saveDev': true });
+        this.npmInstall(['svg-sprite-generator'], { 'saveDev': true });
+        this.npmInstall(['svgo'], { 'saveDev': true });        
+        this.npmInstall(['archiver'], { 'saveDev': true });
+        this.npmInstall(['del'], { 'saveDev': true });
+        this.npmInstall(['wp-pot'], { 'saveDev': true });
+        this.npmInstall(['wiredep'], { 'saveDev': true });
+      }
+    }
+  },
+
+  end: {
+    endMessage: function endMessage() {
+      this.log(chalk.green('\nAll Done!!\n------------------------\n'));
+
+      if (this.props.gulpsetup) {
+        this.log('\nRun ' + chalk.green('gulp') + ' to start the development and ' + chalk.green('gulp build') + ' to create a zip file in ' + chalk.white('dist/' + this.props.themeslug + '.zip') + ' ready for production.');
+      }
+    }
+  },
+});
