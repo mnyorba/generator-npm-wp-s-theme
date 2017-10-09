c035b65ce41ab780049d4c9a95b5fff8c3eba63f
mnyorba <mnyorba@gmail.com>
2017-10-09 16:16:03 +0300

    add gulpfile.js for copy files

---
+++ app/templates/_gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp         = require('gulp');
+
+var js_files     = ['js/*.js', '!js/*.min.js', '!js/lib/**/*.js'];
+var build_files = [
+  '**',
+  '!node_modules',
+  '!node_modules/**',
+  '!bower_components',
+  '!bower_components/**',
+  '!dist',
+  '!dist/**',
+  '!sass',
+  '!sass/**',
+  '!.git',
+  '!.git/**',
+  '!package.json',
+  '!bower.json',
+  '!**/*.arj',
+  '!**/*.rar',
+  '!**/*.zip',
+  '!.gitignore',
+  '!gulpfile.js',
+  '!.editorconfig',
+  '!.jshintrc'
+];
+
+gulp.task('build-copy', function() {
+  return gulp.src(build_files)
+    .pipe(gulp.dest('dist/<%= package_name %>'));
+});
+
+gulp.task('build-zip', function() {
+  return gulp.src('dist/**/*')
+    .pipe(zip('<%= package_name %>.zip'))
+    .pipe(gulp.dest('dist'));
+});
