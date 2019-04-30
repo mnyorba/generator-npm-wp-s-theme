0727aac3a4bd190799298be0b27eaa1d1deed577
mnyorba <mnyorba@gmail.com>
2019-04-30 19:23:58 +0300

    change fs.rename

--- app/index.js
+++ app/index.js
@@ -292,7 +292,12 @@ module.exports = class extends Generator {
 								}
 							});
 
-							fs.rename(filePath, './languages/' + _this.themeSlug + '.pot');
+							// fs.rename(filePath, './languages/' + _this.themeSlug + '.pot');
+							fs.rename(filePath, './languages/' + _this.themeSlug + '.pot', (err) => {
+								if (err) throw err;
+								console.log('renamed complete');
+								this.log(chalk.green('Renamed complete!'));
+							});
 
 							next();
 						});
