fb104d9d18af16cfe31d27d464ea21c7ccbbea25
mnyorba <mnyorba@gmail.com>
2019-04-30 21:09:32 +0300

    change fs.rename

--- app/index.js
+++ app/index.js
@@ -287,7 +287,6 @@ module.exports = class extends Generator {
 								}
 							});
 
-							fs.rename(filePath, './languages/' + _this.themeSlug + '.pot');
 							fs.rename(filePath, './languages/' + _this.themeSlug + '.pot', (err) => {
 								if (err) throw err;
 								console.log(chalk.green('Renamed complete'));
